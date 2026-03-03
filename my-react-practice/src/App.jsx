15f78bdf18c66f4a4146a9ac02ffc446ebd16fc9
dlGuiri <144573339+dlGuiri@users.noreply.github.com>
2026-03-03 18:01:45 +0800

    first commit

---
+++ my-react-practice/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import "./App.css"
+
+export default function App() {
+  return (
+    <>
+      <h1>App Components here</h1>
+      <Header />
+      <MainContent />
+    </>
+  )
+}
