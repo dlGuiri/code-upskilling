e2818d70660e8f16ebef085757cfa93a24730665
dlGuiri <144573339+dlGuiri@users.noreply.github.com>
2026-03-05 22:06:28 +0800

    Added entry content image for Travel Journal

--- my-react-practice/src/components/MainContent.jsx
+++ my-react-practice/src/components/MainContent.jsx
@@ -4,6 +4,11 @@ export default function MainContent() {
             <div className="entry-image">
                 <img src="/Mountain.png" alt="Mountain Image" />
             </div>
+            <div className="entry-content">
+                <img src="/marker.png" alt="location marker" className="location"/>
+                <span className="country">Japan</span>
+                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" className="map-link">View on Google Maps</a>
+            </div>        
         </main>
     )
 }
